dd491b85b886a530792da029af3acffd081767b1
Alex Rodionov <p0deje@gmail.com>
2014-07-16 23:16:52 +0700

    Refactoring YARD::Doctest module

--- lib/yard-doctest.rb
+++ lib/yard-doctest.rb
@@ -9,80 +9,79 @@ require 'yard/doctest/version'
 
 module YARD
   module Doctest
+    extend self
 
-    class << self
-      #
-      # Configures YARD doctest.
-      #
-      # @yield [self]
-      #
-      def configure
-        yield self
-      end
+    #
+    # Configures YARD doctest.
+    #
+    # @yield [self]
+    #
+    def configure
+      yield self
+    end
 
-      #
-      # Passed block called before each example
-      # or specific tests based on passed name.
-      #
-      # It is evaluated in the same context as example.
-      #
-      # @param [String] test
-      # @param [Proc] blk
-      #
-      def before(test = nil, &blk)
-        hooks[:before] << {test: test, block: blk} if block_given?
-      end
+    #
+    # Passed block called before each example
+    # or specific tests based on passed name.
+    #
+    # It is evaluated in the same context as example.
+    #
+    # @param [String] test
+    # @param [Proc] blk
+    #
+    def before(test = nil, &blk)
+      hooks[:before] << {test: test, block: blk} if block_given?
+    end
 
-      #
-      # Passed block called after each example
-      # or specific tests based on passed name.
-      #
-      # It is evaluated in the same context as example.
-      #
-      # @param [String] test
-      # @param [Proc] blk
-      #
-      def after(test = nil, &blk)
-        hooks[:after] << {test: test, block: blk} if block_given?
-      end
+    #
+    # Passed block called after each example
+    # or specific tests based on passed name.
+    #
+    # It is evaluated in the same context as example.
+    #
+    # @param [String] test
+    # @param [Proc] blk
+    #
+    def after(test = nil, &blk)
+      hooks[:after] << {test: test, block: blk} if block_given?
+    end
 
-      #
-      # Passed block called after all examples and
-      # evaluated in the different context from examples.
-      #
-      # It actually just sends block to `Minitest.after_run`.
-      #
-      # @param [Proc] blk
-      #
-      def after_run(&blk)
-        Minitest.after_run &blk
-      end
+    #
+    # Passed block called after all examples and
+    # evaluated in the different context from examples.
+    #
+    # It actually just sends block to `Minitest.after_run`.
+    #
+    # @param [Proc] blk
+    #
+    def after_run(&blk)
+      Minitest.after_run &blk
+    end
 
-      #
-      # Adds definition of test to be skipped.
-      #
-      # @param [Array<String>] test
-      #
-      def skip(test)
-        skips << test
-      end
+    #
+    # Adds definition of test to be skipped.
+    #
+    # @param [Array<String>] test
+    #
+    def skip(test)
+      skips << test
+    end
 
-      #
-      # Array of tests to be skipped.
-      # @api private
-      #
-      def skips
-        @skips ||= []
-      end
+    #
+    # Array of tests to be skipped.
+    # @api private
+    #
+    def skips
+      @skips ||= []
+    end
 
-      #
-      # Returns hash with arrays of before/after hooks.
-      # @api private
-      #
-      def hooks
-        @hooks ||= {}.tap do |hash|
-          hash[:before], hash[:after] = [], []
-        end
+    #
+    # Returns hash with arrays of before/after hooks.
+    # @api private
+    #
+    def hooks
+      @hooks ||= {}.tap do |hash|
+        hash[:before], hash[:after] = [], []
       end
     end
 
